a65eb754beabac5feee6a1a4efd9a8954a79fb3d
sannnann131 <66421065+sannnann131@users.noreply.github.com>
2020-06-24 21:59:31 +0900

    Asynchronous-communication

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
+
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
@@ -15,5 +16,6 @@ module ChatSpace
       g.test_framework false
     end
     config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
   end
 end
