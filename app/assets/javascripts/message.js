da486b7a34e146fe05ea353a216ca4220cd542d8
sannnann131 <66421065+sannnann131@users.noreply.github.com>
2020-06-25 15:10:39 +0900

    Asynchronous-communication-correction

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message){
     if ( message.image ) {
       var html =
-       `<div class="message">
+        `<div class="message">
           <div class="upper-message">
             <div class="upper-message__user-name">
               ${message.user_name}
@@ -39,27 +39,27 @@ $(function(){
       return html;
     };
   }
-$('#new_message').on('submit', function(e){
-  e.preventDefault();
-  var formData = new FormData(this);
-  var url = $(this).attr('action')
-  $.ajax({
-    url: url,
-    type: "POST",
-    data: formData,
-    dataType: 'json',
-    processData: false,
-    contentType: false
-  })
-   .done(function(data){
-     var html = buildHTML(data);
-     $('.messages').append(html);
-     $('form')[0].reset();
-     $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
-     $('submit-btn').prop('disabled', false);
-   })
-   .fail(function() {
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+      .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('form')[0].reset();
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      $('submit-btn').prop('disabled', false);
+      })
+    .fail(function() {
     alert("メッセージ送信に失敗しました");
-});
-})
+    });
+  })
 });
